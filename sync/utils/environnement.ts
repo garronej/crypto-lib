5acd3eae6fb2eae51abc7d155a6ffdb05b6a5111
Joseph Garrone <joseph.garrone.gj@gmail.com>
2019-12-05 15:58:31 +0100

    cosmetic fix

--- sync/utils/environnement.ts
+++ sync/utils/environnement.ts
@@ -6,62 +6,62 @@ declare const require: any;
 
 
 export type Environnement = {
-        type: "BROWSER" | "LIQUID CORE" | "NODE" | "REACT NATIVE";
-        isMainThread: boolean | undefined;
+    type: "BROWSER" | "LIQUID CORE" | "NODE" | "REACT NATIVE";
+    isMainThread: boolean | undefined;
 }
 
 export const environnement: Environnement = (() => {
 
-        if (typeof navigator !== "undefined" && navigator.product === "ReactNative") {
-
-                return {
-                        "type": "REACT NATIVE" as const,
-                        "isMainThread": true
-                };
-
-        }
-
-        if (typeof window !== "undefined") {
-                return {
-                        "type": "BROWSER" as const,
-                        "isMainThread": true
-                };
-        } if (typeof self !== "undefined" && !!self.postMessage) {
-                return {
-                        "type": "BROWSER" as const,
-                        "isMainThread": false
-                };
-        }
-
-        const isNodeCryptoAvailable = (() => {
-
-                try {
-
-                        require("crypto" + "");
-
-                } catch{
-
-                        return false;
-
-                }
-
-                return true;
-
-        })();
-
-        if (isNodeCryptoAvailable) {
-
-                //NOTE: We do not check process.send because browserify hide it.
-                return {
-                        "type": "NODE" as const,
-                        "isMainThread": undefined
-                };
-
-        }
+    if (typeof navigator !== "undefined" && navigator.product === "ReactNative") {
 
         return {
-                "type": "LIQUID CORE" as const,
-                "isMainThread": true
+            "type": "REACT NATIVE" as const,
+            "isMainThread": true
         };
+
+    }
+
+    if (typeof window !== "undefined") {
+        return {
+            "type": "BROWSER" as const,
+            "isMainThread": true
+        };
+    } if (typeof self !== "undefined" && !!self.postMessage) {
+        return {
+            "type": "BROWSER" as const,
+            "isMainThread": false
+        };
+    }
+
+    const isNodeCryptoAvailable = (() => {
+
+        try {
+
+            require("crypto" + "");
+
+        } catch{
+
+            return false;
+
+        }
+
+        return true;
+
+    })();
+
+    if (isNodeCryptoAvailable) {
+
+        //NOTE: We do not check process.send because browserify hide it.
+        return {
+            "type": "NODE" as const,
+            "isMainThread": undefined
+        };
+
+    }
+
+    return {
+        "type": "LIQUID CORE" as const,
+        "isMainThread": true
+    };
 
 })();
